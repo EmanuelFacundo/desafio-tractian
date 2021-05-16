e254df3f54ed9d82b3e704218850f645d09f51a8
Emanuel <emanuel.facundo14@gmail.com>
2021-05-16 20:31:42 -0300

    Asset

--- src/components/Asset/index.tsx
+++ src/components/Asset/index.tsx
@@ -1,10 +1,45 @@
 import { propsAssets } from "./types";
 
-export default function Asset(props:propsAssets){
-  console.log(props.asset)
-  return(
-    <div>
-      
+import "./style.scss"
+
+export default function Asset(props: propsAssets) {
+  const asset = props.asset
+
+  function status() {
+    if (asset.status === "inAlert") {
+      return "Em Alerta"
+    } else if (asset.status === "inDowntime") {
+      return "Em Parada"
+    } else {
+      return "Em Operação"
+    }
+  }
+
+  function renderSensors() {
+    return asset.sensors.map((sensor, index) => {
+      return (
+        <b key={index}>
+          {sensor}
+        </b>
+      )
+    })
+  }
+
+  return (
+    <div className="asset">
+      <h2>{props.company.name}</h2>
+      <h3>{props.unity.name}</h3>
+      <div className="informations">
+        <div className="text">
+          <p>Ativo: <b>{asset.name}</b> </p>
+          <p>Modelo: <b>{asset.model}</b> </p>
+          <p>Status: <b className={asset.status}>{status()}</b> </p>
+          <p>Sensor: {renderSensors()} </p>
+        </div>
+        <div className="visual">
+          <img src={asset.image} alt="Ativo" />
+        </div>
+      </div>
     </div>
   )
 }
