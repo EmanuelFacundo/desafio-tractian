cb13721d8c0dbd6226d3b5d1f8a345b8d986a07e
Emanuel <emanuel.facundo14@gmail.com>
2021-05-20 21:35:58 -0300

    Graphics

--- src/components/Company/index.tsx
+++ src/components/Company/index.tsx
@@ -1,6 +1,8 @@
 import React, { ChangeEvent } from 'react'
 import { AnyAction, bindActionCreators, Dispatch } from 'redux'
 import { connect } from 'react-redux'
+import Highcharts from 'highcharts/highstock';
+import HighchartsReact from 'highcharts-react-official';
 
 import { getDB } from '../Home/reducer/action'
 import { stateType } from '../Home/reducer/types'
@@ -28,6 +30,94 @@ class Company extends React.Component<stateCompanyType, stateProps>{
     name: ""
   }
 
+  optionsg1 = {
+    chart: {
+      type: 'column'
+    },
+    title: {
+      text: 'Saúde dos Ativos'
+    },
+    xAxis: {
+      type: 'category',
+      labels: {
+        rotation: 0,
+        style: {
+          fontSize: '10px',
+          fontFamily: 'Roboto'
+        }
+      },
+      title: {
+        text: 'Temperatura Máxima (°c)'
+      }
+    },
+    yAxis: {
+      min: 0,
+      title: {
+        text: 'Saúde (%)'
+      }
+    },
+    legend: {
+      enabled: false
+    },
+    series: [{
+      name: 'Saúde',
+      data: [
+        ["", 0],
+      ],
+      dataLabels: {
+        enabled: true,
+        rotation: -90,
+        color: '#000000',
+        align: 'right',
+        format: '{point.y:.2f}', // one decimal
+        y: 10, // 10 pixels down from the top
+        style: {
+          fontSize: '10px',
+          fontFamily: 'Verdana, sans-serif'
+        }
+      }
+    }]
+  }
+
+  optionsg2 = {
+    chart: {
+      type: 'column'
+    },
+    title: {
+      text: 'Coletas Uptime(Ligada)'
+    },
+    xAxis: {
+      categories: [''],
+      labels: {
+        rotation: 0,
+        style: {
+          fontSize: '10px',
+          fontFamily: 'Roboto'
+        }
+      }
+    },
+    yAxis: {
+      title: {
+        text: 'Total Coletas / Total de Horas'
+      }
+    },
+    plotOptions: {
+      line: {
+        dataLabels: {
+          enabled: true
+        },
+        enableMouseTracking: true
+      }
+    },
+    series: [{
+      name: 'Total de Coletas',
+      data: [0]
+    },{
+      name: 'Total de Horas de Coletas',
+      data: [0]
+    }]
+  }
+
   constructor(props: stateCompanyType) {
     super(props)
 
@@ -52,7 +142,15 @@ class Company extends React.Component<stateCompanyType, stateProps>{
 
   renderAssets(index: number) {
     return this.props.assets.map(asset => {
-      if (asset.name.match(this.assetSearch)) {
+      if(index === 1){
+        const totalUptime = asset.metrics.totalUptime.toFixed(2)
+        this.optionsg1.series[0].data.push([asset.name, asset.healthscore])
+        this.optionsg2.xAxis.categories.push(asset.name)
+        this.optionsg2.series[0].data.push(asset.metrics.totalCollectsUptime)
+        this.optionsg2.series[1].data.push(parseFloat(totalUptime))
+      }
+
+      if (asset.name.match(this.assetSearch) && asset.unitId === index) {
         return (
           <section key={asset.id}>
             <Asset asset={asset} />
@@ -67,6 +165,7 @@ class Company extends React.Component<stateCompanyType, stateProps>{
   renderUnits() {
     return this.props.units.map((unit, index) => {
       if (unit.name.match(this.unitSearch)) {
+        // console.log(unit.id)
         return (
           <div key={unit.id} className="units">
             <div className="nav">
@@ -77,7 +176,7 @@ class Company extends React.Component<stateCompanyType, stateProps>{
               </div>
             </div>
             <section className="assets">
-              {this.renderAssets(index)}
+              {this.renderAssets(unit.id)}
             </section>
           </div>
         )
@@ -88,6 +187,11 @@ class Company extends React.Component<stateCompanyType, stateProps>{
   }
 
   render() {
+    
+    this.optionsg1.series[0].data.pop()
+    this.optionsg2.xAxis.categories.pop()
+    this.optionsg2.series[0].data.pop()
+    this.optionsg2.series[1].data.pop()
 
     this.assetsCompany = this.props.assets.filter(asset => {
       return asset.companyId === this.company.id ? asset : null
@@ -111,17 +215,29 @@ class Company extends React.Component<stateCompanyType, stateProps>{
                 />
                 <button></button>
               </span>
-            <span className="input">
-              <input
-                placeholder="Pesquisar ativo..."
-                type="text"
-                onChange={this.setAssetSearch}
-              />
-              <button></button>
-            </span>
+              <span className="input">
+                <input
+                  placeholder="Pesquisar ativo..."
+                  type="text"
+                  onChange={this.setAssetSearch}
+                />
+                <button></button>
+              </span>
             </div>
           </div>
           <span className="bar"></span>
+          <section className="graphic">
+            <HighchartsReact
+              highcharts={Highcharts}
+              // constructorType={'stockChart'}
+              options={this.optionsg1}
+            />
+            <HighchartsReact
+              highcharts={Highcharts}
+              // constructorType={'stockChart'}
+              options={this.optionsg2}
+            />
+          </section>
           {this.renderUnits()}
         </div>
       )
